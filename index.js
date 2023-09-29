68987dbdbb1f427189a4005654b969bc5eb2e310
your_name <nomanwahid123@gmail.com>
2023-09-29 23:17:40 +0500

    second commit

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const express = require('express')
-const router = require('./routes/routes')
-const app = express()
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express");
+const router = require("./routes/routes");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_STR);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
 
-
-mongoose.connect(process.env.MONGO_STR,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-console.log("Connected successfully");
-});
+// mongoose.connect(process.env.MONGO_STR, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//   console.log("Connected successfully");
+// });
 
 // Add Access Control Allow Origin headers
 // app.use((req, res, next) => {
@@ -38,10 +46,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-  
-app.use('/', router)
+app.use("/", router);
 
-
-app.listen(process.env.PORT, () => {
-    console.log('server is active')
+connectDB().then(() => {app.listen(process.env.PORT, () => {
+  console.log("server is active");
+});
 })
